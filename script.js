b60088584a65b5626d090942514476c6e314a238
Shafin23 <shafinahnam89@gmail.com>
2023-01-20 15:41:03 +0600

    hiii

---
+++ script.js
@@ -0,0 +1,25 @@
+var menu = document.getElementById("mm");
+var ul = document.getElementById("nn");
+var ul2=document.getElementById("mn");
+var counter = 0;
+
+if(screen.width<=576){
+    menu.style= "display:block;";
+    ul.style="display:none";
+    console.log(screen.width);  
+}
+
+
+menu.addEventListener("click",function(){ 
+    
+    counter=counter+1;
+    if(counter%2==0){
+       
+        ul2.style="display:none";
+    }
+    else{
+        ul2.style="display:block";
+    }
+
+})
+
